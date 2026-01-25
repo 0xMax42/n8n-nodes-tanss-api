879e3377c7f3b48ba4e43fc87b3965df51e6bb69
Max P. <mail@0xMax42.io>
2026-01-25 19:46:38 +0100

    feat(tanss): add quirk for id required in path and body

--- nodes/tanss/lib/quirks.ts
+++ nodes/tanss/lib/quirks.ts
@@ -10,4 +10,11 @@ export const ApiQuirks = {
 	 * Therefore, the behaviour can be switched using this constant.
 	 */
 	requiresIdOnCreate: false,
+	/**
+	 * Some endpoints require the (employee) ID to be present
+	 * both in the path as well as in the body of the request.
+	 * This is rather unusual and likely an error in the API design.
+	 * Therefore, the behaviour can be switched using this constant.
+	 */
+	requiresIdInPathAndBody: false,
 } as const;
