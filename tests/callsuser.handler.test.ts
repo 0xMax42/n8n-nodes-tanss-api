b5ad998568911055c2bdb2c2d4fa8cef2f3254e2
Max P. <mail@0xMax42.io>
2026-01-28 15:36:03 +0100

    test(callsuser): clean up handler test imports and comments

--- tests/callsuser.handler.test.ts
+++ tests/callsuser.handler.test.ts
@@ -1,9 +1,7 @@
-/* tests/callsuser.handler.test.ts */
 import path from 'node:path';
 import { defineHandlerFixtureSuite } from './handlerFixtures/fixtureRunner';
 import { loadFixturesFromDir } from './handlerFixtures/loadFixtures';
 
-// Import the real handler here:
 import { handleCallsUser } from '../nodes/tanss/sub/callsuser';
 
 const fixturesDir = path.join(__dirname, 'fixtures', 'callsuser');
